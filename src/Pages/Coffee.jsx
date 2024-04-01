9f85d8e081089e11f82aacd4f8593cd62935f362
Saleh545 <syusifzade545@gmail.com>
2024-04-01 23:32:37 +0400

    Coffee size create

--- src/Pages/Coffee.jsx
+++ src/Pages/Coffee.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link, useParams } from "react-router-dom";
+import { Link, NavLink, useParams } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Footer from "../components/footer/Footer";
 import "../styles/coffee.css";
 import { IoIosInformationCircleOutline } from "react-icons/io";
-
 
 const Coffee = () => {
   const [coffee, setCoffee] = useState([]);
@@ -37,6 +36,7 @@ const Coffee = () => {
             </Link>
             <span> {item.name} </span>
           </div>
+
           <div className="coffee-item">
             <div className="coffee-img">
               <img src={item.img} alt="Coffee" />
@@ -44,13 +44,34 @@ const Coffee = () => {
             <div className="coffee-text">
               <h3>{item.name}</h3>
               {item.sizes.map((size, index) => (
-                  <h4 key={index}> </h4>
+                <h4 key={index}> </h4>
               ))}
-              <h4>330 calories <span><IoIosInformationCircleOutline /></span></h4>
+              <h4>
+                330 calories{" "}
+                <span>
+                  <IoIosInformationCircleOutline />
+                </span>
+              </h4>
             </div>
           </div>
         </div>
       ))}
+
+      <div className="coffe-options">
+        <div className="container">
+          <div className="coffee-flex">
+            <div className="coffe-left">
+              <h5>Size options</h5>
+              <ul>
+                <li><NavLink><img src="	https://www.starbucks.com/app-assets/76b8892b0db8f5d411988fe1bbbe4141.svg" alt="" /><p>Tall</p><span>12 fl oz</span></NavLink></li>
+                <li><NavLink><img src="	https://www.starbucks.com/app-assets/2920fb2a8c34d3ddb95ad262872526e9.svg" alt="" /><p>Grande</p><span>12 fl oz</span></NavLink></li>
+                <li><NavLink><img src="https://www.starbucks.com/app-assets/55e7819f7cf8e1959ec35e680d46d9a9.svg" alt="" /><p>Venti</p><span>12 fl oz</span></NavLink></li>
+                <li><NavLink><img src="https://www.starbucks.com/app-assets/3abf3fc78365ef0b59bbfd0ecd1c8490.svg" alt="" /><p>Trenta</p><span>12 fl oz</span></NavLink></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
